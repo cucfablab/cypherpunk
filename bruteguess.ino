31a98b1ea1dc8399addfa87177f3606bd8d8562c
Colten Jackson <jazzyjackson@gmail.com>
2019-05-30 18:15:57 -0500

    Update bruteguess.ino

--- bruteguess.ino
+++ bruteguess.ino
@@ -8,11 +8,10 @@
   long password8;
   long password16; 
   long password32; 
-
-
-  int the8bitTime = 0;
-  int the16bitTime = 0;
-  int the32bitTime = 0;
+  long password128a; 
+  long password128b;
+  long password128c;
+  long password128d;
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,38 +24,67 @@ void setup() {
   password8 = pickPassword(8);
   password16 = pickPassword(16);
   password32 = pickPassword(32);
-  
+  password128a = pickPassword(8);
+  password128b = pickPassword(8);
+  password128c = pickPassword(8);
+  password128d = pickPassword(8);
 
   // for each password, 
   // start a timer, crack the password, 
   // then stop the timer, and say how long it took
-  
   int timer = millis();
   long solution8 = bruteForce(password8, 8);
-  the8bitTime = millis() - timer;
+  timer = millis() - timer;
 
+  Serial.println("The password is 8 bits long.");
+  Serial.println("The password was " + String(solution8) + ", it took " + String(timer) + " milliseconds. " +  String(password8) + " \n");
+  delay(4000);
 
   timer = millis();
   long solution16 = bruteForce(password16, 16);
-  the16bitTime = millis() - timer;
+  timer = millis() - timer;
+  Serial.println("The password is 16 bits long.");
+  Serial.println("The password was " + String(solution16) + ", it took " + String(timer) + " milliseconds." +  String(password16) + " \n");
+  delay(4000);
 
   timer = millis();
   long solution32 = bruteForce(password32, 32);
-  the32bitTime = millis() - timer;
-  
-  if(the8bitTime > 0) Serial.print("8 bits took " + String(the8bitTime) + " milliseconds. ");
-  if(the16bitTime > 0) Serial.print("16 bits took " + String(the16bitTime) + " milliseconds. ");
-  if(the32bitTime > 0) Serial.print("32 bits took " + String(the32bitTime) + " milliseconds. ");
-
+  timer = millis() - timer;
+  Serial.println("The password is 32 bits long.");
+  Serial.println("The password was " + String(password32) + ", it took " + String(timer) + " milliseconds. " +  String(password32) + "\n");
+   
 }
 
 void loop() {
+/*  
+  long guessA = 0;
+  long guessB = 0;
+  long guessC = 0;
+  long guessD = 0;
+
+  int timer = millis();
+  for(int i = 0; i < pow(2, 8); i++){
+    for(int j = 0; j < pow(2, 8); j++){
+      for(int k = 0; k < pow(2, 8); k++){
+        for(int l = 0; l < pow(2, 8); l++){
+          if(millis() % 1000 == 0) Serial.println(String(i) + String(j) + String(k) + String(l));
+          if(i == password128a && j == password128b && k == password128c && l == password128d){
+              timer = millis() - timer;
+              Serial.println("The password was " + String(i) + String(j) + String(k) + String(l) + ", it took " + String(timer) + " milliseconds. \n");
+
+          }
+        }
+      }
+    }
+  }
+*/
+
 }
 
 //pickPassword takes the numbers of bits as an argument. Pass 8 and it will return an 8 bit password. 
 
 long pickPassword(int passwordLength) {
-    randomSeed(analogRead(A3));
+    randomSeed(analogRead(0));
     // picks a (semi) random number that 
     // is between 0 and the power of 2 ^ (the length of the password in bits) 
     // e.g. for 4 bits, between 0 and 2*2*2*2. 
@@ -66,23 +94,15 @@ long pickPassword(int passwordLength) {
 
 long bruteForce(long password, int power) {
   long guess = 0;
-  randomSeed(analogRead(A3));
+  randomSeed(analogRead(0));
 
   while(guess != password){
     guess = random(pow(2, power));
-    if(the8bitTime == 0) Serial.println("This guess: " + String(guess));
-    if(millis() % 100 == 0){
-      if(the8bitTime > 0) Serial.print("8 bits took " + String(the8bitTime) + " milliseconds. ");
-      if(the16bitTime > 0) Serial.print("16 bits took " + String(the16bitTime) + " milliseconds. ");
-      Serial.println("This guess: " + String(guess));
-      delay(1);
-    }
+    Serial.println(guess);
   //  guess++;
   // note, when we guess in order… the compiler is toooo clever by half and 
   // it does not go through all the steps because it already knows the answer, so the timing is off.
   }
   return guess;
 }
-
-
 
